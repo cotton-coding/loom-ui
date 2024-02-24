058a3d521e3cfcad0535684e5d4e78e95156d3b7
Wolfgang Rathgeb <W.Rathgeb@dropanote.de>
2024-02-25 00:57:37 +0100

    add storybook

---
+++ packages/storybook/src/stories/chartjs/doughnut.stories.svelte
@@ -0,0 +1,60 @@
+<script lang="ts" context="module">
+
+  import { Template, Story } from '@storybook/addon-svelte-csf';
+  import { Doughnut, Legend } from '@loom-ui/chartjs';
+
+  export const meta = {
+    title: 'Components/Chart.js/Doughnut',
+    component: Image,
+    argTypes: {
+
+    },
+    parameters: {
+      layout: 'centered',
+    },
+  };
+
+</script>
+
+
+<script lang="ts">
+
+
+</script>
+
+<Template let:args>
+  <Doughnut {...args} />
+</Template>
+
+<Story name="Simple" args={{
+		type: 'doughnut',
+		datasets: [
+			{
+				label: 'My First Dataset',
+				data: [300, 50, 100],
+				backgroundColor: [
+					'rgb(255, 99, 132)',
+					'rgb(54, 162, 235)',
+					'rgb(255, 205, 86)',
+				],
+			}
+		]	
+	}} 
+/>
+
+
+<Story name="With legend">
+	<Doughnut type="doughnut" datasets={[
+		{
+			label: 'My First Dataset',
+			data: [300, 50, 100],
+			backgroundColor: [
+				'rgb(255, 99, 132)',
+				'rgb(54, 162, 235)',
+				'rgb(255, 205, 86)',
+			],
+		}
+	]}>
+		<Legend labels={['FEMALE', 'MALE', 'DIVERSE']} />
+	</Doughnut>
+</Story>
