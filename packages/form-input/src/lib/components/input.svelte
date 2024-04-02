731290606a5a9a5649da85189372ded0d35b6519
Wolfgang Rathgeb <W.Rathgeb@dropanote.de>
2024-04-02 15:46:00 +0200

    init from-input

---
+++ packages/form-input/src/lib/components/input.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { getRandomString } from "$lib/utils/random.js";
+
+	export let name: string | null | undefined = undefined;
+	export let id: string = name ? `${name}-${getRandomString()}` : getRandomString(13);
+	export let value: string;
+	export let type: string;
+
+
+</script>
+
+<input  {...$$restProps}>
