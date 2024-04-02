1bb8d5b602182146f1ee3dc0135e3d8844d75a2b
Wolfgang Rathgeb <W.Rathgeb@dropanote.de>
2024-04-02 21:42:44 +0200

    add import first style

--- packages/form-input/src/lib/components/input.svelte
+++ packages/form-input/src/lib/components/input.svelte
@@ -1,12 +1,37 @@
 <script lang="ts">
-	import { getRandomString } from "$lib/utils/random.js";
-
-	export let name: string | null | undefined = undefined;
-	export let id: string = name ? `${name}-${getRandomString()}` : getRandomString(13);
-	export let value: string;
-	export let type: string;
-
-
+	let { ...props } = $props();
 </script>
 
-<input  {...$$restProps}>
+<div>
+	<label><slot /></label>
+	<input {...props} />
+</div>
+
+<style>
+	div {
+		display: flex;
+		flex-direction: row;
+		color: var(--input-text-color);
+	}
+
+	input, label {
+		all: unset;
+		padding: var(--input-spacing-x) var(--input-spacing-y);
+		border: 1px solid var(--border-color-light);
+		background: var(--content-background-color);
+	}
+
+	label {
+		background-color: var(--input-label-background-color);
+	}
+
+	:first-child {
+		border-top-left-radius: var(--border-radius);
+		border-bottom-left-radius: var(--border-radius);
+	}
+
+	:last-child {
+		border-top-right-radius: var(--border-radius);
+		border-bottom-right-radius: var(--border-radius);
+	}
+</style>
